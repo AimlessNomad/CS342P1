89c106dbd3f6fdacc7f443a575a6c66588b04505
AimlessNomad <mascislarson@gmail.com>
2025-09-23 23:01:08 -0500

    3c

--- src/main/java/MyHashMap.java
+++ src/main/java/MyHashMap.java
@@ -4,44 +4,47 @@ import java.util.Iterator;
 public class MyHashMap<T> implements Iterable<T> {
 
     private final ArrayList<GenericQueue<T>> map;
-    private final ArrayList<Integer> hashes;
     private int sz;
 
     public MyHashMap(String key, T value) {
         map = new ArrayList<>(10);
-        hashes = new ArrayList<>();
         this.sz = 0;
         this.put(key, value);
     }
 
     public void put(String key, T value) {
         int hash = key.hashCode();
-        if(hashes.contains(hash)) {
-            this.replace(key, value);
+        GenericQueue<T> queue = map.get(hash % 10);
+        if(queue == null) {
+            map.set(hash % 10, new GenericQueue<>(value));
         } else {
-            hashes.add(hash);
-            GenericQueue<T> queue = map.get(hash % 10);
-            if(queue == null) {
-                map.set(hash % 10, new GenericQueue<>(value));
-            } else {
-                queue.add(value);
+            GenericList.Node<T> curr = queue.getHead();
+            for(int i = 0; i < queue.getLength(); i++) {
+                if (curr.code == hash) {
+                    T temp = curr.data;
+                    curr.data = value;
+                    return;
+                }
+                curr = curr.next;
             }
-            sz++;
+            queue.add(value, hash);
         }
+        sz++;
     }
 
-    public void replace(String key, T value) {
+    public T replace(String key, T value) {
         int hash = key.hashCode();
         GenericQueue<T> queue = map.get(hash % 10);
         GenericList.Node<T> curr = queue.getHead();
         for(int i = 0; i < queue.getLength(); i++) {
             if(curr.code == hash) {
+                T temp = curr.data;
                 curr.data = value;
-                sz++;
-                break;
+                return temp;
             }
             curr = curr.next;
         }
+        return null;
     }
 
     public boolean contains(String key) {
